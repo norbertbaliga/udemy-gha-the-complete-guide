6cafa4c50fb1d9bb9d757c2ef065b80ab0d1bcfd
Norbert Baliga <norbert.baliga@goto.com>
2024-08-12 13:47:04 +0200

    Fix missing string interpolation.

--- data/database.js
+++ data/database.js
@@ -9,7 +9,7 @@ const dbName = process.env.MONGODB_DB_NAME;
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
-console.log('Trying to connect to ${clusterAddress}');
+console.log(`Trying to connect to ${clusterAddress}`);
 
 try {
   await client.connect();
